c19551c4be92a3e11e131c999664e27856a2c5c6
chart21 <chart21@web.de>
2023-05-14 15:09:05 +0200

    updated test.cpp

--- test.cpp
+++ test.cpp
@@ -152,11 +152,11 @@ std::cout << "SHA256 (Intrinsics): " << std::chrono::duration_cast<std::chrono::
 std::cout << "SHA256 (Intrinsics) Throughput: " << 1.024 / std::chrono::duration_cast<std::chrono::milliseconds>(finish5 - finish4).count() * 1000 << "GB/s"<< std::endl;
 #endif
 
-auto a = new uint64_t[1000000000];
-auto b = new uint64_t[1000000000];
-auto c= new uint64_t[1000000000];
+auto a = new uint64_t[1000000];
+auto b = new uint64_t[1000000];
+auto c= new uint64_t[1000000];
 finish6 = std::chrono::high_resolution_clock::now();
-for (int i = 0; i < 1000000000; i++) {
+for (int i = 0; i < 1000000; i++) {
 c[i] = a[i] * b[i];
 }
 finish7 = std::chrono::high_resolution_clock::now();
@@ -164,9 +164,9 @@ delete[] a;
 delete[] b;
 delete[] c;
 
-auto d = new DATATYPE[1000000000];
-auto e = new DATATYPE[1000000000];
-auto f= new DATATYPE[1000000000];
+auto d = new DATATYPE[1000000];
+auto e = new DATATYPE[1000000];
+auto f= new DATATYPE[1000000];
 finish8 = std::chrono::high_resolution_clock::now();
 for (int i = 0; i < 1000000000; i++) {
 f[i] = AND(d[i],e[i]);
@@ -176,16 +176,16 @@ delete[] d;
 delete[] e;
 delete[] f;
 
-auto g = new uint32_t[1000000000];
-auto h = new uint32_t[1000000000];
-auto j= new uint32_t[1000000000];
+auto g = new uint32_t[1000000];
+auto h = new uint32_t[1000000];
+auto j= new uint32_t[1000000];
 finish10 = std::chrono::high_resolution_clock::now();
-for (int i = 0; i < 1000000000; i++) 
+for (int i = 0; i < 1000000; i++) 
     j[i] = g[i] * h[i];
 finish11 = std::chrono::high_resolution_clock::now();
 
 
-for (int i = 0; i < 1000000000; i++) 
+for (int i = 0; i < 1000000; i++) 
     j[i] = g[i] + h[i];
 finish12 = std::chrono::high_resolution_clock::now();
 
@@ -207,10 +207,10 @@ std::cout << "SHA256: " << std::chrono::duration_cast<std::chrono::milliseconds>
 std::cout << "SHA256 Throughput: " << 1.024 / std::chrono::duration_cast<std::chrono::milliseconds>(finish4 - finish3).count() * 1000 << "GB/s"<< std::endl;
 std::cout << "Tested with 16384MB for AES, 1024MB for SHA256" << std::endl;
 
-std::cout << "64-bit Mult Throughput in Gbps: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish7 - finish6).count() / 64 << std::endl;
-std::cout << "AND Throughput" << std::chrono::duration_cast<std::chrono::milliseconds>(finish9 - finish8).count() / sizeof(DATATYPE)*8 << std::endl;
-std::cout << "32-bit Mult Throughput in Gbps: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish11 - finish10).count() / 32 << std::endl;
-std::cout << "32-bit Add Throughput in Gbps: " << std::chrono::duration_cast<std::chrono::milliseconds>(finish12 - finish11).count() / 32 << std::endl;
+std::cout << "64-bit Mult Throughput in Gbps: " << 1000 * std::chrono::duration_cast<std::chrono::milliseconds>(finish7 - finish6).count() / 64 << std::endl;
+std::cout << "AND Throughput" << 1000 * std::chrono::duration_cast<std::chrono::milliseconds>(finish9 - finish8).count() / (sizeof(DATATYPE)*8) << std::endl;
+std::cout << "32-bit Mult Throughput in Gbps: " << 1000 * std::chrono::duration_cast<std::chrono::milliseconds>(finish11 - finish10).count() / 32 << std::endl;
+std::cout << "32-bit Add Throughput in Gbps: " << 1000 * std::chrono::duration_cast<std::chrono::milliseconds>(finish12 - finish11).count() / 32 << std::endl;
 std::cout << m[0] << std::endl;
 std::cout << plain__[0][0] << std::endl;
 std::cout << seed << std::endl;
